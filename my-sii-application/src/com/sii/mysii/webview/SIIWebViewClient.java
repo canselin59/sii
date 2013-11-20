c291c24e72655c6f69931f37f9e364a06dffe959
Quentin Klein <klein.quentin@gmail.com>
2013-11-20 13:35:02 +0100

    Modify: 	The Webview Url handling

--- my-sii-application/src/com/sii/mysii/webview/SIIWebViewClient.java
+++ my-sii-application/src/com/sii/mysii/webview/SIIWebViewClient.java
@@ -1,13 +1,41 @@
 package com.sii.mysii.webview;
 
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class SIIWebViewClient extends WebViewClient {
+	
+	private static final String TAG = "SIIWebViewClient";
+	private ArrayList<String> urlAuthorized;
+	
+	public SIIWebViewClient(String... urlAuthorized){
+		this.urlAuthorized = new ArrayList<String>();
+		if( urlAuthorized != null ){
+			for(int i = 0; i < urlAuthorized.length; i++){
+				Log.d(TAG, "Adding ["+urlAuthorized[i]+"] to the list of white listed urls");
+				this.urlAuthorized.add(urlAuthorized[i]);
+			}
+		}
+	}
 
 	@Override
 	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		view.loadUrl(url);
+		for(String whiteListedUrl : urlAuthorized){
+			if(whiteListedUrl.contains(url)){
+				// If the url is whitelisted, load it directly
+				view.loadUrl(url);
+				return true;
+			}
+		}
+		// If the url is not whitelisted, intent the browser
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		view.getContext().startActivity(i);
 		return true;
 	}
 
